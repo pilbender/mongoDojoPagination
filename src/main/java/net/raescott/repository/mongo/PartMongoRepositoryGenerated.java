51dd656915de2d3a71cf81707143991ac41a396f
Richard Scott Smith <scott.smith@isostech.com>
2014-08-13 09:20:02 -0700

    Switched to the PagingAndSortingRepository for Mongo.

--- src/main/java/net/raescott/repository/mongo/PartMongoRepositoryGenerated.java
+++ src/main/java/net/raescott/repository/mongo/PartMongoRepositoryGenerated.java
@@ -1,13 +1,14 @@
 package net.raescott.repository.mongo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import net.raescott.dto.Part;
 
 /**
  * @author Richard Scott Smith <scott.smith@isostech.com>
  */
-public interface PartMongoRepositoryGenerated extends CrudRepository<Part, String> {
+public interface PartMongoRepositoryGenerated extends PagingAndSortingRepository<Part, String> {
 	// findAll can't be here because it is not a property to generate a method.
 	//public Iterable<Person> findAll(String id);
 	public Part findByPartNumber(String partNumber);
